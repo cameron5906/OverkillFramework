b2360cd0fd80bd1a68eae9e16b1090fa5fa97d9d
Cameron Cooper <cameron@cameron.pw>
2020-08-25 18:14:39 -0400

    Rename Core DriveInputTopic to LocomotionTopic and adjust properties to be more generic

--- Overkill.Core/Topics/LocomotionTopic.cs
+++ Overkill.Core/Topics/LocomotionTopic.cs
@@ -8,10 +8,9 @@ namespace Overkill.Core.Topics
     /// <summary>
     /// Dictates the intent for a user to move the vehicle
     /// </summary>
-    public class DriveInputTopic : IPubSubTopic
+    public class LocomotionTopic : IPubSubTopic
     {
-        public int Throttle { get; set; }
-        public int Steering { get; set; }
-        public bool Brake { get; set; }
+        public float Direction { get; set; }
+        public float Speed { get; set; }
     }
 }
