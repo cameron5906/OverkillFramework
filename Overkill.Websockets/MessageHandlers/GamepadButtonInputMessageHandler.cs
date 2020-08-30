ae0630993c0cdb8343f4cefaa7ee9f5f18f3e88a
Cameron Cooper <cameron@cameron.pw>
2020-08-29 20:46:16 -0400

    Add websocket message handlers for new input messages

---
+++ Overkill.Websockets/MessageHandlers/GamepadButtonInputMessageHandler.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Overkill.Common.Enums;
+using Overkill.Core.Topics;
+using Overkill.PubSub.Interfaces;
+using Overkill.Websockets.Interfaces;
+using Overkill.Websockets.Messages.Input;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overkill.Websockets.MessageHandlers
+{
+    public class GamepadButtonInputMessageHandler : IWebsocketMessageHandler
+    {
+        private IPubSubService pubSub;
+
+        public GamepadButtonInputMessageHandler(IServiceProvider serviceProvider)
+        {
+            pubSub = serviceProvider.GetRequiredService<IPubSubService>();
+        }
+
+        public Task<IWebsocketMessage> Handle(IWebsocketMessage msg)
+        {
+            var gamepadButton = (GamepadButtonInputMessage)msg;
+
+            pubSub.Dispatch(new GamepadButtonInputTopic()
+            {
+                Name = gamepadButton.Name,
+                State = gamepadButton.IsPressed ? InputState.Pressed : InputState.Released
+            });
+
+            return null;
+        }
+    }
+}
---
+++ Overkill.Websockets/MessageHandlers/GamepadTriggerInputMessageHandler.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Overkill.Core.Topics;
+using Overkill.PubSub.Interfaces;
+using Overkill.Websockets.Interfaces;
+using Overkill.Websockets.Messages;
+using Overkill.Websockets.Messages.Input;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overkill.Websockets.MessageHandlers
+{
+    public class GamepadTriggerInputMessageHandler : IWebsocketMessageHandler
+    {
+        private IPubSubService pubSub;
+
+        public GamepadTriggerInputMessageHandler(IServiceProvider serviceProvider)
+        {
+            pubSub = serviceProvider.GetService<IPubSubService>();
+        }
+
+        public Task<IWebsocketMessage> Handle(IWebsocketMessage msg)
+        {
+            var triggerInput = (GamepadTriggerInputMessage)msg;
+
+            pubSub.Dispatch(new GamepadTriggerInputTopic()
+            {
+                Name = triggerInput.Name,
+                Value = triggerInput.Value
+            });
+
+            return null;
+        }
+    }
+}
